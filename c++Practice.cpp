7fb15b74c5601fa4221598e6057a2794545c6898
Maruf Pulok <maruf12@student.sust.edu>
2022-10-21 03:30:05 +0600

    again?

---
+++ c++Practice.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int>v={30,4,12,45,90,46};
+
+    auto it=max_element(v.begin(),v.end());
+    auto it2=min_element(v.begin(),v.end());
+    cout<<(*it)<<" "<<(*it2)<<endl;
+
+    int arr[]={30,4,12,45,90,46};
+    //is this committed?
+    //master h yeh?
+
+    cout<<*max_element(arr,arr+6);
+}
