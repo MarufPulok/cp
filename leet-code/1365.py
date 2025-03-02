9b206a43aa7123566073002c06852a9a6fdb926e
kernelpanic <maruf12@student.sust.edu>
2025-03-02 17:17:52 +0600

    Refactor smallerNumbersThanCurrent function to include alternative solutions with varying time complexities

--- leet-code/1365.py
+++ leet-code/1365.py
@@ -1,7 +1,9 @@
+# O(n^2) solution
 # class Solution:
 #     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
 #         return [len([n for n in nums if n < i]) for i in nums]
 
+# O(nlogn) solution
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
         sorted_nums = sorted(nums)
@@ -13,3 +15,14 @@ class Solution:
         
         return [rank_map[num] for num in nums]
         
+# O(n) solution
+# class Solution:
+#     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+#         count = [0]*101
+#         for num in nums:
+#             count[num] += 1
+        
+#         for i in range(1,101):
+#             count[i] += count[i-1]
+        
+#         return [count[num-1] if num > 0 else 0 for num in nums]
