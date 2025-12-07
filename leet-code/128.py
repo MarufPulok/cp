109403f0efb58e6f79474b612db5ec0bfdf44fd3
kernelpanic <maruf12@student.sust.edu>
2025-12-07 14:39:18 +0600

    Add longestConsecutive function to find the length of the longest consecutive elements sequence

---
+++ leet-code/128.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        nums_set = set(nums)
+        lcs = 1
+
+        for num in nums_set:
+            if num - 1 in nums_set:
+                continue
+            else:
+                current_number = num
+                curr = 1
+                while current_number + 1 in nums_set:
+                    current_number += 1
+                    curr += 1
+                lcs = max(curr,lcs) 
+        
+        return lcs
+
+        
